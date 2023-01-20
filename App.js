6479a72553f2657c26643d30a0b081f062d0c74e
JKosonen <jussi.k.kosonen@gmail.com>
2023-01-20 10:09:25 +0100

    Laskin valmis

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [result, setResult] = useState();
+  
+  const plusButton = () => {
+    setResult(parseInt(first) + parseInt(second));
+
+  }
+  const minusButton = () => {
+    setResult(parseInt(first) - parseInt(second));
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex:1, justifyContent: 'flex-end'}}>
+        <Text> Result: {result}</Text>
+        <TextInput inputmode='numeric' keyboardType='number-pad' style={styles.input} onChangeText={text=> setFirst(text)} value={first} />
+        <TextInput inputmode='numeric' keyboardType='number-pad' style={styles.input} onChangeText={text=> setSecond(text)} value={second} />
+      </View>
+      <View style={{flex:1, flexDirection: 'row', alignItems: 'baseline' }}>
+        <Button title="+" onPress={plusButton} />
+        <Button title="-" onPress={minusButton} />
+      
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +39,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  input : {
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
