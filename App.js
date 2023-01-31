971aa45bcf60d108d4aed5af490b2d001ca71e3c
JKosonen <jussi.k.kosonen@gmail.com>
2023-01-31 14:18:52 +0100

    laskin musitilla ei vielä valmis

--- App.js
+++ App.js
@@ -1,34 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
 
+ 
+
   const [first, setFirst] = useState(0);
   const [second, setSecond] = useState(0);
   const [result, setResult] = useState();
-  
+  const [results, setResults] = useState([]);
+  const [text, setText] = useState('');
+
   const plusButton = () => {
     setResult(parseInt(first) + parseInt(second));
-
+    setText(first + " + " + second + " = " + result)
+    setSecond('');
+    setFirst('');
+    setResults([...results, { key: text }]);
   }
   const minusButton = () => {
     setResult(parseInt(first) - parseInt(second));
+    setText(first + " - " + second + " = " + result)
+    setSecond('');
+    setFirst('');
+    setResults([...results, { key: text }]);
   }
-
+  
 
   return (
     <View style={styles.container}>
-      <View style={{flex:1, justifyContent: 'flex-end'}}>
+
         <Text> Result: {result}</Text>
         <TextInput inputmode='numeric' keyboardType='number-pad' style={styles.input} onChangeText={text=> setFirst(text)} value={first} />
         <TextInput inputmode='numeric' keyboardType='number-pad' style={styles.input} onChangeText={text=> setSecond(text)} value={second} />
-      </View>
-      <View style={{flex:1, flexDirection: 'row', alignItems: 'baseline' }}>
-        <Button title="+" onPress={plusButton} />
-        <Button title="-" onPress={minusButton} />
+        
+        <View style= {styles.operators}>
+          <Button style={styles.button} title="+" onPress={plusButton} />
+          <Button style={styles.button} title="-" onPress={minusButton} />
+        </View>
+
+      <Text> History</Text>
+      <FlatList 
+        data={results}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+      />
       
-      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -39,6 +59,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
     
   },
   input : {
@@ -48,5 +69,15 @@ const styles = StyleSheet.create({
     width: 200,
     borderColor: 'gray',
     borderWidth: 1
+  },
+  operators : {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  buttons: {
+    width: '20%',
+    borderColor: 'black',
+    borderWith: 1,
+    margin: 10, 
   }
 });
